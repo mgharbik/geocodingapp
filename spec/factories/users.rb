4a4c4282f71b23bd50df7c4f7dff2c5f71001cd7
Gharbi Mohammed <mgharbik@gmail.com>
2025-03-14 06:52:30 +0100

    Revert "feat: add geocoder gem and geocode the user address"
    This reverts commit 78c59699be39c573028b3709123d6ed1b5b7a136.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :user do
     email { "test@example.com" }
-    password { "password123" }
-    password_confirmation { "password123" }
-    street { "Irisring 1" }
+    street { "123 Main St" }
     city { "Berlin" }
-    zip { "13089" }
+    zip { "12345" }
   end
 end
